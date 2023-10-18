92cc4e7f0fcfe5b8333ad8cf206a13b9d8c51961
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2023-10-17 23:55:03 -0300

    conditional case if v2score or v3score exists

--- src/js/render/vulnerabilities.js
+++ src/js/render/vulnerabilities.js
@@ -34,21 +34,34 @@ function fillVulnerabilitiesTable(result) {
 	const vulnerabilitiesTableBody = vulnerabilitiesTable.querySelector("tbody");
 	result.Vulnerabilities.forEach((vuln) => {
 		const resultTableRow = tableRowTemplate.content.cloneNode(true);
+		
 		const pkgName = resultTableRow.querySelector(".pkg-name");
 		pkgName.textContent = vuln.PkgName;
+		
 		const vulnerability = resultTableRow.querySelector(".vuln");
 		vulnerability.textContent = vuln.VulnerabilityID;
+		
 		const v2Score = resultTableRow.querySelector(".v2-score");
 		v2Score.textContent = vuln.CVSS.nvd.V2Score;
-		const v3Score = resultTableRow.querySelector(".v3-score");
-		v3Score.textContent = vuln.CVSS.nvd.V3Score;
+
+		if (vuln.CVSS.nvd.V2Score || vuln.CVSS.redhat.V2Score) {
+			const v2Score = resultTableRow.querySelector(".v2-score");
+			v2Score.textContent = vuln.CVSS.nvd.V2Score ? vuln.CVSS.nvd.V2Score : vuln.CVSS.redhat.V2Score;
+		}
+		
+		if (vuln.CVSS.nvd.V3Score || vuln.CVSS.redhat.V3Score) {
+			const v3Score = resultTableRow.querySelector(".v3-score");
+			v3Score.textContent = vuln.CVSS.nvd.V3Score ? vuln.CVSS.nvd.V3Score : vuln.CVSS.redhat.V3Score;
+		}
+
 		const vulnTitle = resultTableRow.querySelector(".vuln-title");
-		vulnTitle.textContent = vuln.CVSS.nvd.Title;
+		vulnTitle.textContent = vuln.Title;
+		
 		const severity = resultTableRow.querySelector(".severity");
 		severity.textContent = vuln.Severity;
+		
 		const pkgVersion = resultTableRow.querySelector(".pkg-version");
 		pkgVersion.textContent = vuln.InstalledVersion;
-		
 		
 		const links = resultTableRow.querySelector(".links");
 		if (vuln.References) {
