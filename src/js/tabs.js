2e0ec4ea55ae01e970203ceda1123b41e6a1bf8d
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2023-10-19 21:49:16 -0300

    filtering by type

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,24 +1,32 @@
 function removeActiveClasses() {
   const tabButtons = document.querySelectorAll(".tab-button");
-  const tabContents = document.querySelectorAll(".tab-content");
+  const cvesContent = document.getElementsByClassName("tables");
+  const secretsContent = document.getElementsByClassName("secret-results");
 
   tabButtons.forEach((button) => button.classList.remove("active"));
-  tabContents.forEach((content) => (content.style.display = "none"));
+  Object.values(cvesContent).forEach((table) => (table.style.display = "none"));
+  Object.values(secretsContent).forEach(
+    (table) => (table.style.display = "none")
+  );
 }
 
 function filterByType() {
   const cvesTab = document.getElementById("cves-tab");
   const packagesTab = document.getElementById("packages-tab");
   const secretsTab = document.getElementById("secrets-tab");
+  const miscTab = document.getElementById("misc-tab");
 
-  const cvesContent = document.getElementById("cves-content");
-  const packagesContent = document.getElementById("packages-content");
-  const secretsContent = document.getElementById("secrets-content");
+  const cvesContent = document.getElementsByClassName("tables");
+  const packagesContent = document.getElementById("pkgs-table");
+  const secretsContent = document.getElementsByClassName("secret-results");
+  const miscContent = document.getElementById("misc-content");
 
   cvesTab.addEventListener("click", () => {
     removeActiveClasses();
     cvesTab.classList.add("active");
-    cvesContent.style.display = "block";
+    Object.values(cvesContent).forEach(
+      (table) => (table.style.display = "block")
+    );
   });
 
   packagesTab.addEventListener("click", () => {
@@ -30,7 +38,15 @@ function filterByType() {
   secretsTab.addEventListener("click", () => {
     removeActiveClasses();
     secretsTab.classList.add("active");
-    secretsContent.style.display = "block";
+    Object.values(secretsContent).forEach(
+      (table) => (table.style.display = "block")
+    );
+  });
+
+  miscTab.addEventListener("click", () => {
+    removeActiveClasses();
+    miscTab.classList.add("active");
+    miscContent.style.display = "block";
   });
 }
 
